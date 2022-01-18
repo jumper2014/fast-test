aacd7c916e53ab2aeb6939a318c5f1635fd75061
ijumper <ijumper@163.com>
2022-01-18 10:59:05 +0800

    增加ehcache示例

---
+++ fast-test-framework/src/test/java/com/zyt/utils/cache/EhcacheServiceImpl.java
@@ -0,0 +1,63 @@
+package com.zyt.utils.cache;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EhcacheServiceImpl implements EhcacheService {
+
+    // value的值和ehcache.xml中的配置保持一致
+    @Cacheable(value = "HelloWorldCache", key = "#param")
+    public String getTimestamp(String param) {
+        Long timestamp = System.currentTimeMillis();
+        return timestamp.toString();
+    }
+
+    @Cacheable(value = "HelloWorldCache", key = "#key")
+    public String getDataFromDB(String key) {
+        System.out.println("从数据库中获取数据...");
+        return key + ":" + String.valueOf(Math.round(Math.random() * 1000000));
+    }
+
+    @CacheEvict(value = "HelloWorldCache", key = "#key")
+    public void removeDataAtDB(String key) {
+        System.out.println("从数据库中删除数据");
+    }
+
+    @CachePut(value = "HelloWorldCache", key = "#key")
+    public String refreshData(String key) {
+        System.out.println("模拟从数据库中加载数据");
+        return key + "::" + String.valueOf(Math.round(Math.random() * 1000000));
+    }
+
+    // ------------------------------------------------------------------------
+    // 将缓存保存到名称为UserCache中，键为"user:"字符串加上userId值，如 'user:1'
+    @Cacheable(value = "UserCache", key = "'user:' + #userId")
+    public User findById(String userId) {
+        System.out.println("模拟从数据库中查询数据");
+        return new User(1, "mengdee");
+    }
+
+    // 将缓存保存进UserCache中，并当参数userId的长度小于12时才保存进缓存，默认使用参数值及类型作为缓存的key
+    // 保存缓存需要指定key，value， value的数据类型，不指定key默认和参数名一样如："1"
+    @Cacheable(value = "UserCache", condition = "#userId.length()<12")
+    public boolean isReserved(String userId) {
+        System.out.println("UserCache:" + userId);
+        return false;
+    }
+
+    //清除掉UserCache中某个指定key的缓存
+    @CacheEvict(value = "UserCache", key = "'user:' + #userId")
+    public void removeUser(String userId) {
+        System.out.println("UserCache remove:" + userId);
+    }
+
+    //allEntries：true表示清除value中的全部缓存，默认为false
+    //清除掉UserCache中全部的缓存
+    @CacheEvict(value = "UserCache", allEntries = true)
+    public void removeAllUser() {
+        System.out.println("UserCache delete all");
+    }
+}
---
+++ fast-test-framework/src/test/java/com/zyt/utils/cache/EhcacheTest.java
@@ -0,0 +1,99 @@
+package com.zyt.utils.cache;
+
+import com.zyt.App;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ContextConfiguration(locations = {"classpath:ehcache.xml"})
+@EnableCaching  // 在启动类前加上@EnableCaching注解；这样的话，启动类启动时会去启动缓存启动器。
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class EhcacheTest {
+
+    @Autowired //@Autowired 是通过 byType 的方式去注入的， 使用该注解，要求接口只能有一个实现类。
+    private EhcacheService ehcacheService;
+
+    // 有效时间是5秒，第一次和第二次获取的值是一样的，因第三次是5秒之后所以会获取新的值
+    @Test
+    public void testTimestamp() throws InterruptedException {
+        System.out.println("第1次调用：" + ehcacheService.getTimestamp("param"));
+        Thread.sleep(2000);
+        System.out.println("2秒之后调用：" + ehcacheService.getTimestamp("param"));
+        Thread.sleep(4000);
+        System.out.println("再过4秒之后调用：" + ehcacheService.getTimestamp("param"));
+
+    }
+
+    @Test
+    public void testCache() {
+        String key = "zhangsan";
+        String value = ehcacheService.getDataFromDB(key); // 从数据库中获取数据...
+        ehcacheService.getDataFromDB(key);  // 从缓存中获取数据，所以不执行该方法体
+        ehcacheService.removeDataAtDB(key); // 从数据库中删除数据
+        ehcacheService.getDataFromDB(key);  // 从数据库中获取数据...（缓存数据删除了，所以要重新获取，执行方法体）
+    }
+
+    @Test
+    public void testPut() {
+        // PUT因为可以执行方法提,可以用来做缓存的更新
+        String key = "mengdee";
+        ehcacheService.refreshData(key);  // 模拟从数据库中加载数据
+        String data = ehcacheService.getDataFromDB(key);
+        System.out.println("data:" + data); // data:mengdee::103385
+
+        ehcacheService.refreshData(key);  // 模拟从数据库中加载数据
+        String data2 = ehcacheService.getDataFromDB(key);
+        System.out.println("data2:" + data2);   // data2:mengdee::180538
+    }
+
+    @Test
+    public void testFindById() {
+        ehcacheService.findById("2"); // 模拟从数据库中查询数据
+        ehcacheService.findById("2");
+    }
+
+    @Test
+    public void testIsReserved() {
+        ehcacheService.isReserved("123");  // 满足条件缓存了
+        ehcacheService.isReserved("123");  // 因为缓存了，所以不执行
+        ehcacheService.isReserved("1234567890123");  // 不满足条件，未缓存了
+        ehcacheService.isReserved("1234567890124");  // 因为未缓存，所以执行
+    }
+
+    @Test
+    public void testRemoveUser() {
+        // 线添加到缓存
+        ehcacheService.findById("1");
+
+        // 再删除
+        ehcacheService.removeUser("1");
+
+        // 如果不存在会执行方法体
+        ehcacheService.findById("1");
+    }
+
+    @Test
+    public void testRemoveAllUser() {
+        ehcacheService.findById("1");
+        ehcacheService.findById("2");
+
+        ehcacheService.removeAllUser();
+
+        ehcacheService.findById("1");
+        ehcacheService.findById("2");
+
+//      模拟从数据库中查询数据
+//      模拟从数据库中查询数据
+//      UserCache delete all
+//      模拟从数据库中查询数据
+//      模拟从数据库中查询数据
+    }
+
+
+}
