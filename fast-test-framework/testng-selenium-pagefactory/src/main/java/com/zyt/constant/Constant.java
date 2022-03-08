b51622afa817395769152bd8a1a61ba31baca195
ijumper <ijumper@163.com>
2022-03-08 15:18:40 +0800

    add testng-selenium-pagefactory project

---
+++ fast-test-framework/testng-selenium-pagefactory/src/main/java/com/zyt/constant/Constant.java
@@ -0,0 +1,6 @@
+package com.zyt.constant;
+
+public class Constant {
+    public final static String URL = "http://www.baidu.com";
+    public final static String DRIVER_PATH = "c:/kit/chromedriver.exe";
+}
